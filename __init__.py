1329aa173546e2da3ea014ddd178bfe05d649600
Zuellni <123005779+Zuellni@users.noreply.github.com>
2023-05-15 22:18:39 +0200

    Fix config

--- __init__.py
+++ __init__.py
@@ -24,26 +24,31 @@ config = {
 
 path = Path(__file__)
 config_path = path.with_name("config.json")
-req_path = path.with_name("requirements.txt")
 
 if config_path.is_file():
 	with open(config_path, "r") as f:
 		dict = json.load(f)
-		
+
 		for key, val in dict.items():
 			if key in config:
-				for sub_key, sub_val in config[key].items():
-					if sub_key in config:
+				for sub_key, sub_val in dict[key].items():
+					if sub_key in config[key]:
 						config[key][sub_key] = sub_val
 
 with open(config_path, "w") as f:
 	json.dump(config, f, indent = "\t", separators = (",", ": "))
-		
+
 if config["Settings"]["Update"]:
 	import subprocess
 
 	quiet = "-q" if config["Settings"]["Quiet Update"] else ""
-	subprocess.run(f"git pull {quiet} -C {path}")
+	req_path = path.with_name("requirements.txt")
+	git_path = path.parent
+	
+	print("\n[\033[94mZuellni\033[0m]: Updating repository...")
+	subprocess.run(f"git -C {git_path} pull {quiet}")
+	
+	print("[\033[94mZuellni\033[0m]: Installing requirements...")
 	subprocess.run(f"pip install {quiet} --upgrade-strategy only-if-needed -r {req_path}")
 
 if config["Settings"]["Suppress Warnings"]:
@@ -55,6 +60,7 @@ if config["Settings"]["Suppress Warnings"]:
 	filterwarnings("ignore", category = UserWarning, message = "TypedStorage is deprecated")
 	filterwarnings("ignore", category = FutureWarning, message = "The `reduce_labels` parameter is deprecated")
 	filterwarnings("ignore", category = UserWarning, message = "You seem to be using the pipelines sequentially on GPU")
+
 	logging.getLogger("xformers").addFilter(lambda r: "A matching Triton is not available" not in r.getMessage())
 	transformers_logging.set_verbosity_error()
 	diffusers_logging.set_verbosity_error()
@@ -67,5 +73,6 @@ for key, val in config["Load Nodes"].items():
 			if cls.__module__ == module.__name__:
 				node = f"{cls.__module__}.{name}"
 				disp = f"{key} {name.replace('_', ' ')}"
+
 				NODE_CLASS_MAPPINGS[node] = cls
 				NODE_DISPLAY_NAME_MAPPINGS[node] = disp
