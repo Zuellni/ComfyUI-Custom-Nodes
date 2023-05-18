4d16aba6bef59e800ae685acc64a5b0918e1bf7d
Zuellni <123005779+Zuellni@users.noreply.github.com>
2023-05-18 11:31:20 +0200

    Fix classifier math a bit

--- Nodes/Aesthetic.py
+++ Nodes/Aesthetic.py
@@ -31,22 +31,18 @@ class Loader:
                 "aesthetic": {
                     "pipe": pipe(aesthetic, "cafeai/cafe_aesthetic"),
                     "weights": [0.0, 1.0],
-                    "positive": 1,
                 },
                 "style": {
                     "pipe": pipe(style, "cafeai/cafe_style"),
-                    "weights": [1.0, 1.0, 0.5, 0.0, 0.0],
-                    "positive": 3,
+                    "weights": [1.0, 0.75, 0.5, 0.0, 0.0],
                 },
                 "waifu": {
                     "pipe": pipe(waifu, "cafeai/cafe_waifu"),
                     "weights": [0.0, 1.0],
-                    "positive": 1,
                 },
                 "age": {
                     "pipe": pipe(age, "nateraw/vit-age-classifier"),
-                    "weights": [0.0, 0.5, 1.0, 0.75, 0.5, 0.0, 0.0, 0.0, 0.0],
-                    "positive": 4,
+                    "weights": [0.25, 0.5, 1.0, 0.75, 0.5, 0.0, 0.0, 0.0, 0.0],
                 },
             },
         )
@@ -95,20 +91,21 @@ class Select:
                 if pipe:
                     labels = pipe.model.config.id2label
                     weights = model["weights"]
-                    num = len(weights)
-                    map = {labels[i]: weights[i] for i in range(num)}
-                    items = pipe(image, top_k=num)
-                    score += sum(v["score"] * map[v["label"]] / model["positive"] for v in items)
+                    w_len = len(weights)
+                    w_sum = sum(weights)
+                    w_map = {labels[i]: weights[i] for i in range(w_len)}
+                    items = pipe(image, top_k=w_len)
+                    score += sum(v["score"] * w_map[v["label"]] / w_sum for v in items)
 
             scores[index] = score
 
         scores = sorted(scores.items(), key=lambda k: k[1], reverse=True)
-        images = [images[score[0]] for score in scores[:count]]
+        images = [images[v[0]] for v in scores[:count]]
         images = torch.stack(images)
 
         if latents:
             latents = latents["samples"]
-            latents = [latents[score[0]] for score in scores[:count]]
+            latents = [latents[v[0]] for v in scores[:count]]
             latents = {"samples": torch.stack(latents)}
 
         return (images, latents)
