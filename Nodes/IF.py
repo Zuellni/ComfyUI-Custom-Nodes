66b5dea3f602698ac6c89ca7e37db21312568ebc
Zuellni <123005779+Zuellni@users.noreply.github.com>
2023-05-26 00:03:44 +0200

    Use default settings for 4bit quant

--- Nodes/IF.py
+++ Nodes/IF.py
@@ -57,9 +57,7 @@ class Loader:
                 "T5-4": BitsAndBytesConfig(
                     device_map="auto",
                     load_in_4bit=True,
-                    bnb_4bit_quant_type="nf4",
                     bnb_4bit_use_double_quant=True,
-                    bnb_4bit_compute_dtype=torch.float16,
                 ),
                 "T5-8": BitsAndBytesConfig(
                     device_map="auto",
